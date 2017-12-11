ebe715c5172fa2d0049b510246776c344fee665c
Vadym P <developer1.reliefapps@gmail.com>
2017-12-11 11:26:07 +0100

    R redis config

--- web_R/worker.py
+++ web_R/worker.py
@@ -2,8 +2,8 @@ import os
 from celery import Celery
 
 env=os.environ
-CELERY_BROKER_URL=env.get('CELERY_BROKER_URL','redis://localhost:6380'),
-CELERY_RESULT_BACKEND=env.get('CELERY_RESULT_BACKEND','redis://localhost:6380')
+CELERY_BROKER_URL=env.get('CELERY_BROKER_URL','redis_r://localhost:6380'),
+CELERY_RESULT_BACKEND=env.get('CELERY_RESULT_BACKEND','redis_r://localhost:6380')
 
 
 celery= Celery('R_tasks',
