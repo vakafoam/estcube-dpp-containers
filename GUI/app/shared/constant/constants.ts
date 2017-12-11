8633b32d84e39ce740dbfa3ec006800be142da07
Vadym P <developer1.reliefapps@gmail.com>
2017-12-11 09:18:13 +0100

    Another Redis for R

--- GUI/app/shared/constant/constants.ts
+++ GUI/app/shared/constant/constants.ts
@@ -9,9 +9,11 @@ export const URLS = {
     //'R': 'http://localhost:5000/task',
     // 'SciPy': 'http://dpp-broker-scipy:8263/task',
     // 'Scilab': 'http://dpp-broker-scilab:8264/task',
-    //'SciPy': 'http://unicorn.estcube.eu:8260',
-    'SciPy': 'http://192.168.99.100:8260',
-    'R': 'http://192.168.99.100:8261',
+    'SciPy': 'http://unicorn.estcube.eu:8260',
+    'R': 'http://unicorn.estcube.eu:8261',
+
+    // 'SciPy': 'http://192.168.99.100:8260',
+    // 'R': 'http://192.168.99.100:8261',
 
 
 }
--- worker_R/tasks.py
+++ worker_R/tasks.py
@@ -7,8 +7,8 @@ from taskModel import Task
 
 
 env=os.environ
-CELERY_BROKER_URL=env.get('CELERY_BROKER_URL','redis://localhost:6379'),
-CELERY_RESULT_BACKEND=env.get('CELERY_RESULT_BACKEND','redis://localhost:6379')
+CELERY_BROKER_URL=env.get('CELERY_BROKER_URL','redis://localhost:6380'),
+CELERY_RESULT_BACKEND=env.get('CELERY_RESULT_BACKEND','redis://localhost:6380')
 
 
 celery= Celery('R_tasks',
