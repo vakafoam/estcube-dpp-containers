dc9e703a3276275eee56caca1de90e500cbd3070
Vadym P <developer1.reliefapps@gmail.com>
2017-12-20 10:36:38 +0100

    Scilab plotting fixes

--- Scilab/Scilab/Test/1.sce
+++ Scilab/Scilab/Test/1.sce
@@ -1,8 +1,18 @@
-scf(0);
-plot2d();
-//PDF export
-filename='foo';
-xs2pdf(0,filename);
-xs2pdf(gcf(),filename);
+// Define your independent values
+x = [0 : 0.2 : 4*%pi];
 
-https://help.scilab.org/doc/5.5.2/en_US/driver.html
+
+
+driver('PNG');
+xinit('plot.png');
+
+// Define your functions and plot.
+// The default color is black
+plot2d3(x, sin(x))
+// 5 at the end means red line
+plot2d3(x, sin(2*x), 5)
+// Superimpose a red envelope on the red lines
+plot2d(x, sin(2*x), 5)
+
+xend();
+exit();
--- worker_Scilab/tasks.py
+++ worker_Scilab/tasks.py
@@ -28,7 +28,8 @@ def sl_script(task):
     # folder = task.folder
     folder = task.getFolder()
     os.chdir(folder)
-    command = ("../../scilab-6.0.0/bin/scilab-adv-cli -f {0} -nwni".format(task.name))
+    # command = ("../../scilab-6.0.0/bin/scilab-adv-cli -f {0} -nwni".format(task.name))
+    command = ("../../scilab-6.0.0/bin/scilab-adv-cli -f {0} -nogui".format(task.name))
     out = run (command, task.outName, task.logName)
     # out = run (command, task.getOutPath(), task.getLogPath())
     return out
